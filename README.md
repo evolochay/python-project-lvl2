473198498238948f8f810c6d6aa64b0a5556ec41
volochay <evgenia@volochay.info>
2022-02-15 19:37:32 +0100

    3 step done and asciinema added

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/foxy-chay/python-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/foxy-chay/python-project-lvl2/actions)
+
+### Asciinema
+
+[![asciicast](https://asciinema.org/a/wW455f73KFxoF2OvwmYZZtwg4.svg)](https://asciinema.org/a/wW455f73KFxoF2OvwmYZZtwg4)
--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -3,20 +3,27 @@ from xmlrpc.client import Boolean
 
 
 def generate_diff(file_path1, file_path2):
+    result = '{\n'
     first_file = json.load(open(file_path1))
     second_file = json.load(open(file_path2))
-    result = '{' + '\n'
-    for i in first_file.keys() & second_file.keys():
-        if first_file[i] == second_file[i]:
-            result = result + '   ' + i + ': ' + str(lower_case(first_file[i])) + '\n'
-        else:
-            result = result + ' + ' + i + ': ' + str(lower_case(second_file[i])) + '\n'
-            result = result + ' - ' + i + ': ' + str(lower_case(first_file[i])) + '\n'
-    for i in first_file.keys() - second_file.keys():
-         result = result + ' - ' + i + ': ' + str(lower_case(first_file[i])) + '\n'
-    for i in second_file.keys() - first_file.keys():
-         result = result + ' + ' + i + ': ' + str(lower_case(second_file[i])) + '\n'
-    result = result + '}'
+    all_keys = sorted(list(first_file.keys() | second_file.keys()))
+    two_keys = first_file.keys() & second_file.keys()
+    deleted_keys = first_file.keys() - second_file.keys()
+    added_keys = second_file.keys() - first_file.keys()
+     
+    for key in all_keys:
+        if key in two_keys:
+            if first_file[key] == second_file[key]:
+                result += '    {}: {}\n'.format(key, lower_case(first_file[key]))
+            else:
+                result += '  - {}: {}\n'.format(key, lower_case(first_file[key]))
+                result += '  + {}: {}\n'.format(key, lower_case(second_file[key]))
+        elif key in deleted_keys:
+             result += '  - {}: {}\n'.format(key, lower_case(first_file[key]))
+        elif key in added_keys:
+            result += '  + {}: {}\n'.format(key, lower_case(second_file[key]))
+
+    result += '}'
 
     return result
 
