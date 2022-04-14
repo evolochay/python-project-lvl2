541d134f34c4a7cf3822f0e4fe4c11a9ee8c6c56
volochay <evgenia@volochay.info>
2022-04-14 23:17:15 +0200

    test-files changed

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,21 +1,20 @@
-from tests.fixtures.result_stylish import EXPECTED_STRING
 from gendiff.gendiff import generate_diff
-
-
-def test_json_files():
-    assert EXPECTED_STRING == generate_diff(
-        './tests/fixtures/file1.json', './tests/fixtures/file2.json', 'stylish')
-
-
-def test_yaml_files():
-    assert EXPECTED_STRING == generate_diff(
-        './tests/fixtures/file1.yml', './tests/fixtures/file2.yml', 'stylish')
 
 
 def read_file(file_path):
     with open(file_path, 'r') as file:
         result = file.read()
     return result
+
+
+def test_json_files():
+    assert read_file('./tests/fixtures/result_stylish.txt') == generate_diff(
+        './tests/fixtures/file1.json', './tests/fixtures/file2.json', 'stylish')
+
+
+def test_yaml_files():
+    assert read_file('./tests/fixtures/result_stylish.txt') == generate_diff(
+        './tests/fixtures/file1.yml', './tests/fixtures/file2.yml', 'stylish')
 
 
 def test_plain_format():
