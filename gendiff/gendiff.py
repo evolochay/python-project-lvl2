7264b64b96901dbb0899c9fd190fa6ebc228d4d7
volochay <evgenia@volochay.info>
2022-04-26 22:45:57 +0200

    problem with parser and tests fixed (maybe)

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -5,9 +5,11 @@ from gendiff.comparator import comparing
 
 
 def generate_diff(path1, path2, format=STYlISH_FORMAT):
-    first_dict = get_data(path1)
+    first_file_extension = get_file_extension(path1)
+    second_file_extension = get_file_extension(path2)
+    first_dict = parsing(path1, first_file_extension)
     print(first_dict)
-    second_dict = get_data(path2)
+    second_dict = parsing(path2, second_file_extension)
     print(second_dict)
     diff = comparing(first_dict, second_dict)
     return format_diff(diff, format)
@@ -16,8 +18,3 @@ def generate_diff(path1, path2, format=STYlISH_FORMAT):
 def get_file_extension(some_data):
     file_extension = some_data.split('.')[-1]
     return file_extension
-
-
-def get_data(pathfile):
-    with open(pathfile, 'r') as data:
-        return parsing(data, get_file_extension(pathfile))
--- gendiff/parser.py
+++ gendiff/parser.py
@@ -2,16 +2,10 @@ import json
 import yaml
 
 
-def parsing(some_data, format):
-    try:
-        if format in ('yml', 'yaml'):
-            result_dict = yaml.load(some_data, Loader=yaml.FullLoader)
-        elif format == 'json':
-            result_dict = json.load(some_data)
-
-        if result_dict is None:
-            raise TypeError
-    except (TypeError, yaml.parser.ParserError):
-        return False
-    else:
-        return result_dict
+def parsing(some_data, file_extension):
+    result_dict = {}
+    if file_extension in ('yml', 'yaml'):
+        result_dict = yaml.load(open(some_data), Loader=yaml.FullLoader)
+    elif file_extension == 'json':
+        result_dict = json.load(open(some_data))
+    return result_dict
