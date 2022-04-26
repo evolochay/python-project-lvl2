f87a2f8e4064a6dbfa064b59b1d98b9c2cc02ed5
volochay <evgenia@volochay.info>
2022-04-26 22:51:23 +0200

    problem with parser and tests fixed (maybe)

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,20 +1,13 @@
-from gendiff.parser import parsing
+from gendiff.parser import parsing, get_file_extension
 from gendiff.formatters.format import STYlISH_FORMAT
 from gendiff.formatters.format import format_diff
 from gendiff.comparator import comparing
 
 
 def generate_diff(path1, path2, format=STYlISH_FORMAT):
-    first_file_extension = get_file_extension(path1)
-    second_file_extension = get_file_extension(path2)
-    first_dict = parsing(path1, first_file_extension)
-    print(first_dict)
-    second_dict = parsing(path2, second_file_extension)
-    print(second_dict)
+    first_data_extension = get_file_extension(path1)
+    second_data_extension = get_file_extension(path2)
+    first_dict = parsing(path1, first_data_extension)
+    second_dict = parsing(path2, second_data_extension)
     diff = comparing(first_dict, second_dict)
     return format_diff(diff, format)
-
-
-def get_file_extension(some_data):
-    file_extension = some_data.split('.')[-1]
-    return file_extension
--- gendiff/parser.py
+++ gendiff/parser.py
@@ -3,9 +3,14 @@ import yaml
 
 
 def parsing(some_data, file_extension):
-    result_dict = {}
     if file_extension in ('yml', 'yaml'):
         result_dict = yaml.load(open(some_data), Loader=yaml.FullLoader)
     elif file_extension == 'json':
         result_dict = json.load(open(some_data))
     return result_dict
+
+
+
+def get_file_extension(some_data):
+    file_extension = some_data.split('.')[-1]
+    return file_extension
